b7e4bd7bb50b6e4468961596a1fcce7e67b4791a
Robby Wauters <robbywauters@gmail.com>
2014-05-05 17:18:19 +0200

    Goo lib update & Camera control scripts

--- js/loadScene.js
+++ js/loadScene.js
@@ -1,184 +1,155 @@
 require([
 	'goo/entities/GooRunner',
 	'goo/fsmpack/statemachine/StateMachineSystem',
-	'goo/addons/howler/systems/HowlerSystem',
+	'goo/entities/systems/HtmlSystem',
+	'goo/timelinepack/TimelineSystem',
 	'goo/loaders/DynamicLoader',
 
+	'js/CanvasWrapper',
+	'js/checkBrowser',
+
 	'goo/fsmpack/StateMachineComponentHandler',
-	'goo/fsmpack/MachineHandler'
+	'goo/fsmpack/MachineHandler',
+	'goo/timelinepack/TimelineComponentHandler',
+	'goo/quadpack/QuadComponentHandler'
 ], function (
 	GooRunner,
 	StateMachineSystem,
-	HowlerSystem,
-	DynamicLoader
+	HtmlSystem,
+	TimelineSystem,
+	DynamicLoader,
+	CanvasWrapper,
+	checkBrowser
 ) {
 	'use strict';
 
-	function disabledWebGL() {
-
-		var gl = null;
-		var canvas = document.createElement('canvas');
-		var e;
-		try {
-			gl = canvas.getContext('webgl');
-		}
-		catch (_error) {
-			e = _error;
-			gl = null;
-		}
-
-		if (gl === null) {
-			try {
-				gl = canvas.getContext('experimental-webgl');
-			}
-			catch (_error) {
-				e = _error;
-				gl = null;
-			}
-		}
-
-		if (gl === null && window.WebGLRenderingContext) {
-			return true;
-		} else {
-			return false;
-		}
-	};
-
-	function init() {
-
-		// If you try to load a scene without a server, you're gonna have a bad time
-		if (window.location.protocol==='file:') {
-			alert('You need to run this webpage on a server. Check the code for links and details.');
-			return;
+	function setup(goo, loader) {
+			// Application code goes here!
+			// gooRunner.world.by.name('Cam_Left').first().setAsMainCamera();
+			// console.log(gooRunner.world.by.name('Cam_Left').first().scriptComponent.scripts[0]);
+			camController["Front"] = goo.world.by.name('Cam_Center_Far').first();
+			camController["Right"] = goo.world.by.name('Cam_Right').first();
+			camController["Back L"] = goo.world.by.name('Cam_Rear_Right').first();
+			camController["Back R"] = goo.world.by.name('Cam_Rear_Left').first();
+			camController["Left"] = goo.world.by.name('Cam_Left').first();
+			console.log("Cams loaded");
+			console.log(camController);
+			console.log(camController["Front"].scriptComponent.scripts[0].parameters.speed);
 
 			/*
-
-			Loading scenes uses AJAX requests, which require that the webpage is accessed via http. Setting up
-			a web server is not very complicated, and there are lots of free options. Here are some suggestions
-			that will do the job and do it well, but there are lots of other options.
-
-			- Windows
-
-			There's Apache (http://httpd.apache.org/docs/current/platform/windows.html)
-			There's nginx (http://nginx.org/en/docs/windows.html)
-			And for the truly lightweight, there's mongoose (https://code.google.com/p/mongoose/)
-
-			- Linux
-			Most distributions have neat packages for Apache (http://httpd.apache.org/) and nginx
-			(http://nginx.org/en/docs/windows.html) and about a gazillion other options that didn't
-			fit in here.
-			One option is calling 'python -m SimpleHTTPServer' inside the unpacked folder if you have python installed.
-
-
-			- Mac OS X
-
-			Most Mac users will have Apache web server bundled with the OS.
-			Read this to get started: http://osxdaily.com/2012/09/02/start-apache-web-server-mac-os-x/
-
+			To get a hold of entities, one can use the World's selection functions:
+			var allEntities = gooRunner.world.getEntities();                  // all
+			var entity      = gooRunner.world.by.name('EntityName').first();  // by name
 			*/
-		}
+	}
 
-		// Make sure user is running Chrome/Firefox and that a WebGL context works
-		var isChrome, isFirefox, isIE, isOpera, isSafari, isCocoonJS;
-		isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-			isFirefox = typeof InstallTrigger !== 'undefined';
-			isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
-			isChrome = !!window.chrome && !isOpera;
-			isIE = false || document.documentMode;
-			isCocoonJS = navigator.appName === "Ludei CocoonJS";
-		if (!(isFirefox || isChrome || isSafari || isCocoonJS || isIE === 11)) {
-			alert("Sorry, but your browser is not supported.\nGoo works best in Google Chrome or Mozilla Firefox.\nYou will be redirected to a download page.");
-			window.location.href = 'https://www.google.com/chrome';
-		} else if (!window.WebGLRenderingContext) {
-			alert("Sorry, but we could not find a WebGL rendering context.\nYou will be redirected to a troubleshooting page.");
-			window.location.href = 'http://get.webgl.org/troubleshooting';
-		} else if (disabledWebGL() === true) {
-			alert('You seem to have WebGL disabled, redirecting to a helpful page.');
-			window.location.href = 'http://create.gootechnologies.com/webgl-disabled';
-		} else {
-			// Preventing browser peculiarities to mess with our controls
-			document.body.addEventListener('touchstart', function(event) {
-				event.preventDefault();
-			}, false);
-			// Loading screen callback
-			var progressCallback = function (handled, total) {
-				var loadedPercent = (100*handled/total).toFixed();
-				var loadingOverlay = document.getElementById("loadingOverlay");
-				var progressBar = document.getElementById("progressBar");
-				var progress = document.getElementById("progress");
-				var loadingMessage = document.getElementById("loadingMessage");
-				loadingOverlay.style.display = "block";
-				loadingMessage.style.display = "block";
-				progressBar.style.display = "block";
-				progress.style.width = loadedPercent + "%";
-			};
+	function init() {
+		// Prevent browser peculiarities to mess with our controls.
+		document.body.addEventListener('touchstart', function(event) {
+			event.preventDefault();
+		}, false);
 
-			// Create typical Goo application
-			var goo = new GooRunner({
-				antialias: true,
-				manuallyStartGameLoop: true
-			});
+		// Check that the browser supports webGL
+		checkBrowser();
 
-			var fsm = new StateMachineSystem(goo);
-			goo.world.setSystem(fsm);
-			goo.world.setSystem(new HowlerSystem());
+		// Init the GooEngine
+		var gooRunner = initGoo();
 
-			// The loader takes care of loading the data
-			var loader = new DynamicLoader({
-				world: goo.world,
-				rootPath: 'res'
-			});
+		// Load the project
+		loadProject(gooRunner).then(function(loader) {
+			gooRunner.world.process();
+			return setup(gooRunner, loader);
+		}).then(function() {
+			// Hide the loading overlay.
+			document.getElementById('loading-overlay').style.display = 'none';
+			CanvasWrapper.show();
+			gooRunner.world.process();
+			CanvasWrapper.resize();
+			// Start the rendering loop!
+			gooRunner.startGameLoop();
 
-			loader.load('root.bundle', {
-				preloadBinaries: true,
-				progressCallback: progressCallback
-			}).then(function(result) {
+			gooRunner.renderer.domElement.focus();
+		}).then(null, function(e) {
+			// If something goes wrong, 'e' is the error message from the engine.
+			alert('Failed to load project: ' + e);
+		});
 
-				// Grab the first project in the bundle.
-				var bundleKeys = Object.keys(result);
-				var projectIds = bundleKeys.filter(function(k) {
-					return /\.project$/.test(k);
-				});
-				var projectId = projectIds[0];
-				if (!projectId) {
-					alert('Error: No project in bundle'); // Should never happen
-					return null;
+	}
+
+
+	function initGoo() {
+		// Create typical Goo application.
+		var gooRunner = new GooRunner({
+			antialias: true,
+			manuallyStartGameLoop: true,
+			useDevicePixelRatio: true
+		});
+
+		gooRunner.world.add(new StateMachineSystem(gooRunner));
+		gooRunner.world.add(new HtmlSystem(gooRunner.renderer));
+		gooRunner.world.add(new TimelineSystem());
+
+		return gooRunner;
+	}
+
+
+	function loadProject(gooRunner) {
+		/**
+		 * Callback for the loading screen.
+		 *
+		 * @param  {number} handled
+		 * @param  {number} total
+		 */
+		var progressCallback = function (handled, total) {
+			var loadedPercent = (100 * handled / total).toFixed();
+			var loadingOverlay = document.getElementById("loading-overlay");
+			var progressBar = document.getElementById("progress-bar");
+			var progress = document.getElementById("progress");
+			var loadingMessage = document.getElementById("loading-message");
+
+			loadingOverlay.style.display = "block";
+			loadingMessage.style.display = "block";
+			progressBar.style.display = "block";
+			progress.style.width = loadedPercent + "%";
+		};
+
+		// The loader takes care of loading the data.
+		var loader = new DynamicLoader({
+			world: gooRunner.world,
+			rootPath: 'res'
+		});
+
+		return loader.load('root.bundle', {
+			preloadBinaries: true,
+			progressCallback: progressCallback
+		}).then(function(result) {
+			var project = null;
+
+			// Try to get the first project in the bundle.
+			for (var key in result) {
+				if (/\.project$/.test(key)) {
+					project = result[key];
+					break;
 				}
+			}
 
-				return loader.load(projectId);
+			if (!project || !project.id) {
+				alert('Error: No project in bundle'); // Should never happen.
+				return null;
+			}
 
-			}).then(function() {
+			// Setup the canvas configuration (sizing mode, resolution, aspect
+			// ratio, etc).
+			var scene = result[project.mainSceneRef];
+			var canvasConfig = scene ? scene.canvas : {};
+			CanvasWrapper.setup(gooRunner.renderer.domElement, canvasConfig);
+			CanvasWrapper.add();
+			CanvasWrapper.hide();
 
-				// This code will be called when the project has finished loading.
 
-				goo.renderer.domElement.id = 'goo';
-				document.body.appendChild(goo.renderer.domElement);
 
-				goo.world.process();
-
-				// Application code goes here!
-
-				cams.push(goo.world.by.name('Cam_Center_Far').first());
-				cams.push(goo.world.by.name('Cam_Right').first());
-				cams.push(goo.world.by.name('Cam_Rear_Right').first());
-				cams.push(goo.world.by.name('Cam_Rear_Left').first());
-				cams.push(goo.world.by.name('Fixed Cam').first());
-				console.log(cams);
-				/*
-				To get a hold of entities, one can use the World's selection functions:
-				var allEntities = goo.world.getEntities();                  // all
-				var entity      = goo.world.by.name('EntityName').first();  // by name
-				*/
-
-				// Start the rendering loop!
-				goo.startGameLoop();
-
-			}).then(null, function(e) {
-				// If something goes wrong, 'e' is the error message from the engine.
-				alert('Failed to load project: ' + e);
-			});
-
-		}
+			return loader.load(project.id);
+		});
 	}
 
 	function initEvents(){
@@ -189,11 +160,12 @@ require([
 	}
 
 	function changeCam(e){
-		console.log(cams[e]);
-		cams[e].setAsMainCamera();
+		//console.log(camController[e]);
+		camController[e].setAsMainCamera();
 	}
 
 	var cams = [null];
+	var camController = {};
 
 	init();
 	initEvents();
